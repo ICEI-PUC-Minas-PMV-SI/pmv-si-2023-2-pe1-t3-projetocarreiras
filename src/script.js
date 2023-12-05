828f8260e326616daaa79a9380d04827166c6cb6
Leonardo Junior <104607051+LJ-Leonardo@users.noreply.github.com>
2023-12-05 13:45:37 -0300

    Update script.js - att

--- src/script.js
+++ src/script.js
@@ -19,8 +19,8 @@ const validateInput = ({ target }) => {
 
 function confir_senha() {
     document.querySelector('.senha').style.display = "block"
-    input.setAttribute('disable', '')
-    document.querySelector('.login_button').innerHTML = "LOGIN"
+    input.setAttribute('disabled', '')
+    document.querySelector('.login_button').innerHTML = "ACESSO"
 }
 
 function TelaCriarConta() {
@@ -57,8 +57,8 @@ function Criar_Conta() {
     retorno = 1
   }
   var telefone = document.getElementById("_telefone").value
-  var cpf = document.getElementById("_cpf").value
-  if (validCPF()) {
+  var cnpj = document.getElementById("_cnpj").value
+  if (validCNPJ()) {
     retorno = 1
   }
   var senha = document.getElementById("_senha").value
@@ -71,7 +71,7 @@ function Criar_Conta() {
   } else {
     document.getElementById('_nome').style.border = "2px solid #15c22c"
   }
-  if (senha.length < 6) {
+  if (senha.length < 4) {
     document.getElementById('_senha').style.border = "2px solid #e80c0c"
     retorno++
   } else {
@@ -83,7 +83,7 @@ function Criar_Conta() {
   } else {
     document.getElementById('conf_senha').style.border = "2px solid #15c22c"
   }
-  if (telefone.length < 15) {
+  if (telefone.length < 8) {
     document.getElementById('_telefone').style.border = "2px solid #e80c0c"
     retorno++
   } else {
@@ -97,18 +97,13 @@ function Criar_Conta() {
               nome_ : nome,
               email_ : email,
               tel_ : telefone,
-              cpf_ : cpf,
+              cnpj_ : cnpj,
               senha_ : senha
               };
   
   // Armazenar os dados no Local Storage do Navegador
-  localStorage.setItem(key, JSON.stringify(usuario))
-  
-  localStorage.setItem('id_login', key)
-  const time = setTimeout(abrir, 800)
-    function abrir(){
-      window.location = './pages/loading.html'
-  }
+  localStorage.setItem(key, JSON.stringify(usuario));
+  localStorage.setItem('id_login', key);
 }
 
 function validEmail() {
@@ -139,14 +134,14 @@ function validEmail() {
   return false
 }
 
-function validCPF() {
-  var cpf = document.getElementById("_cpf").value
+function validCNPJ() {
+  var cnpj = document.getElementById("_cnpj").value
 
-  if (cpf.length < 14) {
-    document.getElementById('_cpf').style.border = "2px solid #e80c0c"
+  if (cnpj.length < 13) {
+    document.getElementById('_cnpj').style.border = "2px solid #e80c0c"
     return true
   } else {
-    document.getElementById('_cpf').style.border = "2px solid #15c22c"
+    document.getElementById('_cnpj').style.border = "2px solid #15c22c"
   }
   
   for (let i = 0; i < localStorage.length; i++) {
@@ -159,8 +154,8 @@ function validCPF() {
       user = ""
     }
 
-    if (cpf == user.cpf_) {
-      document.getElementById('_cpf').style.border = "2px solid #e80c0c"
+    if (cnpj == user.cnpj_) {
+      document.getElementById('_cnpj').style.border = "2px solid #e80c0c"
       return true
     }
   }
@@ -189,10 +184,8 @@ function verifica_login() {
       if (senha != "") {
         if (senha == user.senha_) {
           localStorage.setItem('id_login', id)
-          const time = setTimeout(abrir, 800)
-            function abrir(){
-              window.location = './pages/loading.html'
-          }
+          window.location = '/src/html/navegacao/perfil_empresa.html'
+
         } else {
           document.querySelector('.msg1').innerHTML = "Senha incorreta"
           document.querySelector('.senha').value = ""
@@ -208,42 +201,5 @@ function verifica_login() {
   }
 
 }
-// var input_tel = document.querySelector("input[type=tel]");
-var input_tel = document.getElementById('_telefone');
-input_tel.addEventListener('keyup', mask_tel);
-
-// var input_cpf = document.querySelector("input[type=text]");
-var input_cpf = document.getElementById('_cpf');
-input_cpf.addEventListener('keyup', mask_cpf);
-
-function mask_tel(e){
-    var caractere = e.target.value.replace(/\D/g,""); 
-    caractere = caractere.replace(/^(\d\d)(\d)/g,"($1) $2");
-    caractere = caractere.replace(/(\d{5})(\d)/,"$1-$2");
-
-    e.target.value = caractere;
-
-}
-
-function mask_cpf(e){
-    var caractere = e.target.value.replace(/\D/g,"");
-    caractere = caractere.replace(/(\d{3})(\d)/,"$1.$2");
-    caractere = caractere.replace(/(\d{3})(\d)/,"$1.$2");
-    caractere = caractere.replace(/(\d{3})(\d{1,2})$/,"$1-$2");
-
-    e.target.value = caractere;
-
-}
-  
 input.addEventListener('input', validateInput)
 
-
-// --------------- Menu Hamburguer -------------------------- 
-
-function openNav(){
-  document.getElementById("myNav").style.width = '100%'; 
-}
-
-function closeNav(){
-  document.getElementById("myNav").style.width = '0'; 
-}
